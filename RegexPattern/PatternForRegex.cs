2b4beafa91edf99afcff64a314f35a89b96d7412
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-21 17:24:19 -0700

    UC1-ValidFirstName

---
+++ RegexPattern/PatternForRegex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    public class PatternForRegex
+    {
+        public static void ValidFirstName()
+        {
+            Console.WriteLine("Enter first name:");
+            string fname=Console.ReadLine();
+
+            string pattern = "^[A-Z]{1}[a-z]{2,8}$";
+
+            if (Regex.IsMatch(fname, pattern))
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        
+        }
+
+    }
+}
---
+++ RegexPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternForRegex.ValidFirstName();
+
+            Console.ReadLine();
+        }
+    }
+}
